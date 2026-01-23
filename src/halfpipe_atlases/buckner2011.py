84efc03fd216aad8541154065d3dc858f4a36443
Lea Waller <lea@lea.science>
2026-01-23 18:10:20 +0100

    Fix Buckner et al. (2011) download urls

--- src/halfpipe_atlases/buckner2011.py
+++ src/halfpipe_atlases/buckner2011.py
@@ -2,35 +2,47 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
+from pathlib import Path
+from tempfile import mkdtemp
+from zipfile import ZipFile
 import pandas as pd
 from halfpipe import resource as hr
 
 from .merge import AtlasMerge
 
-buckner2011_base_url = (
-    "https://surfer.nmr.mgh.harvard.edu/"
-    "pub/dist/freesurfer/tutorial_packages/OSX/freesurfer/average/"
-    "Buckner_JNeurophysiol11_MNI152/"
+buckner2011_zip = "Buckner_JNeurophysiol11_MNI152.zip"
+buckner2011_zip_url = (
+    "https://surfer.nmr.mgh.harvard.edu/pub/data/Buckner_JNeurophysiol11_MNI152.zip"
 )
-
 buckner2011_17networks_atlas = (
+    "Buckner_JNeurophysiol11_MNI152/"
     "Buckner2011_17Networks_MNI152_FreeSurferConformed1mm_LooseMask.nii.gz"
 )
-buckner2011_17networks_labels = "Buckner2011_17Networks_ColorLUT.txt"
+buckner2011_17networks_labels = (
+    "Buckner_JNeurophysiol11_MNI152/Buckner2011_17Networks_ColorLUT.txt"
+)
 
 
 extra_resources = dict()
-extra_resources[
-    buckner2011_17networks_atlas
-] = f"{buckner2011_base_url}/{buckner2011_17networks_atlas}"
-extra_resources[
-    buckner2011_17networks_labels
-] = f"{buckner2011_base_url}/{buckner2011_17networks_labels}"
+extra_resources[buckner2011_zip] = buckner2011_zip_url
 hr.online_resources.update(extra_resources)
 
 
 def from_buckner2011(merge: AtlasMerge, prefix: str | None = "Buckner2011") -> None:
-    in_labels_file = str(hr.get(buckner2011_17networks_labels))
+    temp = Path(mkdtemp())
+    zip_file = hr.get(buckner2011_zip)
+
+    with ZipFile(zip_file, "r") as zip_file_handle:
+        zip_file_handle.extract(
+            member=buckner2011_17networks_atlas,
+            path=temp,
+        )
+        zip_file_handle.extract(
+            member=buckner2011_17networks_labels,
+            path=temp,
+        )
+
+    in_labels_file = str(temp / buckner2011_17networks_labels)
 
     in_labels_df = pd.read_table(
         in_labels_file, index_col=0, sep=r"\s+", names=["name", "r", "g", "b", "a"]
@@ -40,7 +52,7 @@ def from_buckner2011(merge: AtlasMerge, prefix: str | None = "Buckner2011") -> N
     in_labels = in_labels_df["name"]
 
     merge.from_file(
-        prefix, hr.get(buckner2011_17networks_atlas), in_labels, space="MNI152NLin6Asym"
+        prefix, temp / buckner2011_17networks_atlas, in_labels, space="MNI152NLin6Asym"
     )
 
 
